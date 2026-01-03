ad2c0a3ae9835cfcbf8ecb25196288a1b21ca333
Brandon Hernández <isahecam@gmail.com>
2026-01-03 17:19:44 -0600

    Refactor App component to implement counter functionality with FloatingActionButtons

--- App.tsx
+++ App.tsx
@@ -1,12 +1,36 @@
 import { StatusBar } from 'expo-status-bar';
+import { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import FloatingActionButton from './components/floating-action-button';
 
 export default function App() {
+  const [count, setCount] = useState(0);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    /* SafeAreaView: Asegura que el contenido no se superponga
+    con las áreas no utilizables de la pantalla (notch, barra de estado, etc.) */
+
+    <SafeAreaView style={{ flex: 1 }}>
+      <View style={styles.container}>
+        <Text style={styles.textHuge}>{count}</Text>
+
+        <FloatingActionButton
+          label='Resetar'
+          position='left'
+          onPress={() => setCount(0)}
+          onLongPress={() => setCount(0)}
+        />
+
+        <FloatingActionButton
+          label='+1'
+          onPress={() => setCount(count + 1)}
+          onLongPress={() => setCount(0)}
+        />
+
+        <StatusBar style='auto' />
+      </View>
+    </SafeAreaView>
   );
 }
 
@@ -16,5 +40,10 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+
+  textHuge: {
+    fontSize: 64,
+    fontWeight: 'thin',
   },
 });
